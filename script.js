f5e34da1653c313348a29ffdf9d7289cb2167b42
Ascheraf <achraf.farissi@gmail.com>
2025-03-12 17:46:57 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const myElement = document.querySelector('#myElement');
-  if (myElement) {
-    myElement.addEventListener('click', function () {
-      console.log('Element geklikt!');
-    });
-  }
-});
